1d09ddabb020f8cfe0aea0d532ad36888782b5c6
Chandan Dhamande <virtualassistance.py@gmail.com>
2025-10-22 01:20:08 -0700

    Time: 3 ms (87.93%) | Memory: 18.7 MB (98.44%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,25 +1,23 @@
 from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {i: [] for i in range(numCourses)}
+        adj = {i: [] for i in range(numCourses)}
         indegree = [0] * numCourses
-        
-        for dst, src in prerequisites:
-            graph[src].append(dst)
+        for src, dst in prerequisites:
+            adj[src].append(dst)
             indegree[dst] += 1
         
-        queue = deque([])
-        visited = 0
+        queue = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
-        
+        visited = 0
         while queue:
             node = queue.popleft()
             visited += 1
-            for neighbor in graph[node]:
+            for neighbor in adj[node]:
                 indegree[neighbor] -= 1
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
         return visited == numCourses
-        
+
