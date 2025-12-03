aa5b29d70a74d4115637ffa04549a455ab6984d3
Chandan Dhamande <virtualassistance.py@gmail.com>
2025-12-03 10:57:26 -0800

    Time: 212 ms (35.93%) | Memory: 23.8 MB (12.93%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -13,4 +13,5 @@ class Solution:
                     del memory[fruits[l]]
                 l += 1
             maxLen = max(maxLen, r - l + 1)
-        return maxLen            
+
+        return maxLen
