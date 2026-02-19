d6e50827864df7291cb42a0b41bfbece5105baf5
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-02-18 16:58:35 -0800

    Time: 115 ms (33.14%) | Memory: 23.8 MB (65.94%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,27 +1,28 @@
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
+    def sumSubarrayMins(self, nums: List[int]) -> int:
         result = 0
         stack = []
-        ple = [-1] * len(arr)
-        for i in range(len(arr)):
-            while stack and arr[stack[-1]] > arr[i]:
+        pse = [-1] * len(nums)
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]] > nums[i]:
                 stack.pop()
             if stack:
-                ple[i] = stack[-1]
+                pse[i] = stack[-1]
             stack.append(i)
         
-
         stack = []
-        nse = [len(arr)] * len(arr)
-        for i in range(len(arr) - 1, -1, -1):
-            while stack and arr[stack[-1]] >= arr[i]:
+        nse = [len(nums)] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            while stack and nums[stack[-1]] >= nums[i]:
                 stack.pop()
             if stack:
                 nse[i] = stack[-1]
             stack.append(i)
         
-        for i in range(len(arr)):
-            leftCount = i - ple[i]
-            rightCount =  nse[i] - i
-            result = (result + arr[i] * leftCount * rightCount) % (10**9 + 7)
+        for i in range(len(nums)):
+            leftcount = i - pse[i]
+            rightcount = nse[i] - i
+            result = (result + nums[i] * leftcount * rightcount) % (10**9 + 7)
         return result
+
+        
