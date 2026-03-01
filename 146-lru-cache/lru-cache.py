b1f825e861ac6f6be17f72d44b43dca4fcc728ab
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-02-28 17:01:23 -0800

    Time: 159 ms (18.82%) | Memory: 76.9 MB (73.85%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,52 +1,49 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key=0, val=0):
         self.key = key
         self.val = val
-        self.prev = None
-        self.next = None
-
+        self.prev = self.next = None
+        
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.cap = capacity
+        self.map = {}
+        self.head, self.tail = Node(), Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    def _remove(self, node):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
     
-    def _insert(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def _insertAtMRU(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._insert(node)
-            return node.val
-        return -1
+        if key not in self.map:
+            return -1
+        node = self.map[key]
+        self._remove(node)
+        self._insertAtMRU(node)
+        return node.val
+
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self._remove(self.cache[key])
-        
+        if key in self.map:
+            self._remove(self.map[key])
         node = Node(key, value)
-        self._insert(node)
-        self.cache[key] = node
-        if len(self.cache) > self.capacity:
-            lru = self.tail.prev
+        self.map[key] = node
+        self._insertAtMRU(node)
+        if len(self.map) > self.cap:
+            lru = self.head.next
             self._remove(lru)
-            del self.cache[lru.key]
-
-
+            del self.map[lru.key]
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
