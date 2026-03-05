6a19a776ad51c43e215c20927424f166c4f685fa
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-03-05 15:21:44 -0800

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.28%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -4,20 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def bfs(node):
-            array = []
-            queue = deque([node])
-            while queue:
-                currentnode = queue.popleft()
-                if currentnode:
-                    array.append(currentnode.val)
-                    queue.append(currentnode.left)
-                    queue.append(currentnode.right)
-                else:
-                    array.append(None)
-            return array
-        return bfs(p) == bfs(q)
-        
+        def dfs(p, q):
+            if p is None and q is None:
+                return True
+            if not p or not q or p.val != q.val:
+                return False
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+        return dfs(p, q)
+
+                
