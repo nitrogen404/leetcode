0ed84ea1a37518115681aa0f2fc95d72e6c48c1b
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-03-05 16:41:28 -0800

    Time: 3 ms (20.07%) | Memory: 22.1 MB (38.75%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -8,15 +8,16 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
+        if root.val < key:
             root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
         else:
             if not root.left:
                 return root.right
             if not root.right:
                 return root.left
+            
             successor = root.right
             while successor.left:
                 successor = successor.left
