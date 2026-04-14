739afb8e6f570203aa4ddac4c774d6db52889a1f
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-04-14 14:29:00 -0700

    Time: 136 ms (39.19%) | Memory: 78.7 MB (18.01%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,23 +1,24 @@
 class Node:
-    def __init__(self, key=0, val=0):
+    def __init__(self, key=0, value=0):
         self.key = key
-        self.val = val
-        self.prev = self.next = None
-        
+        self.value = value
+        self.prev = None
+        self.next = None
+    
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
         self.map = {}
-        self.head, self.tail = Node(), Node()
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-    
-    def _remove(self, node):
+
+    def _remove(self, node):  # removes the node from current position
         node.prev.next = node.next
         node.next.prev = node.prev
     
-    def _insertAtMRU(self, node):
+    def _insert_at_MRU(self, node):
         prev = self.tail.prev
         prev.next = node
         node.prev = prev
@@ -27,24 +28,25 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key not in self.map:
             return -1
+        
         node = self.map[key]
         self._remove(node)
-        self._insertAtMRU(node)
-        return node.val
-
+        self._insert_at_MRU(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
             self._remove(self.map[key])
+
         node = Node(key, value)
         self.map[key] = node
-        self._insertAtMRU(node)
-        if len(self.map) > self.cap:
+        self._insert_at_MRU(node)
+        
+        if len(self.map) > self.capacity:
             lru = self.head.next
             self._remove(lru)
             del self.map[lru.key]
         
-
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
