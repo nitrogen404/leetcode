ad76eb918f40fffb985ab7471c16110338519ce0
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-05-20 18:00:45 -0700

    Time: 291 ms (8.92%) | Memory: 20.1 MB (89.56%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,29 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        COLS = len(matrix[0])
-        ROWS = len(matrix)
-        isFirstBlockZero = False
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        for row in range(ROWS):
-            for col in range(COLS):
-                if matrix[row][col] == 0:
-                    matrix[0][col] = 0
-                    if row > 0:
-                        matrix[row][0] = 0
-                    else:
-                        isFirstBlockZero = True
-        
-        for r in range(1, ROWS):
-            if matrix[r][0] == 0:
-                matrix[r] = [0 for _ in range(COLS)]
-        
-        for c in range(COLS):
-            if matrix[0][c] == 0:
-                for r in range(1, ROWS):
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    
+                    for col in range(cols):
+                        if matrix[r][col] != 0:
+                            matrix[r][col] = 'a'
+                    
+                    for row in range(rows):
+                        if matrix[row][c] != 0:
+                            matrix[row][c] = 'a'
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 'a':
                     matrix[r][c] = 0
-        
-        if isFirstBlockZero:
-            matrix[0] = [0 for _ in range(COLS)]
-
-        
+         
