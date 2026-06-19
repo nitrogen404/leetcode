e32436274d070b1a55c9024d454fac5a44a0a7ad
Chandan Dhamande <virtualassistance.py@gmail.com>
2026-06-19 11:48:15 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.63%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -4,6 +4,6 @@ class Solution:
             total = 0
             while num > 0:
                 total += num % 10
-                num //= 10
+                num = num // 10
             num = total
         return num
